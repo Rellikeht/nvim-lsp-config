068439f285208734c68773150e8ca60f85ada5af
Rellikeht <michaelhemperek@gmail.com>
2025-11-23 14:57:10 +0100

    add lsp hover utilities

--- lua/nvim_lsp_config.lua
+++ lua/nvim_lsp_config.lua
@@ -1,18 +1,74 @@
-local function DiagWinPreview()
-  local cw = vim.api.nvim_replace_termcodes("<C-w>", true, true, true)
-  vim.cmd.normal(cw .. "w")
-  vim.cmd.sleep("10m")
+-- TODO document all this
+
+-- inspired by
+-- https://github.com/neovim/neovim/discussions/35953#discussioncomment-14544580
+-- this makes it possible to toggle hover window in insert mode
+local float_wrapper = function(opener, var_name)
+  return function(contents, syntax, opts)
+    local buf_id, win_id = opener(contents, syntax, opts)
+    if not buf_id or not win_id then
+      return buf_id, win_id
+    end
+    if opts.parent_bufnr ~= nil then
+      vim.api.nvim_buf_set_var(
+        opts.parent_bufnr,
+        var_name,
+        win_id
+      )
+    end
+    if opts.callback then
+      opts.callback()
+    end
+    return buf_id, win_id
+  end
+end
+
+local function diag_win_preview(bufnr, var_name)
+  local result, win_id = pcall(vim.api.nvim_buf_get_var, bufnr, var_name)
+  if not result or not win_id then
+    return
+  elseif not vim.api.nvim_win_is_valid(win_id) then
+    vim.api.nvim_buf_set_var(bufnr, var_name, nil)
+    return
+  end
+  vim.api.nvim_set_current_win(win_id)
+  vim.cmd("silent! pclose")
   vim.cmd("silent! pedit")
-  vim.cmd.close()
-  vim.cmd.normal(cw .. "P")
+  vim.api.nvim_win_close(win_id, true)
+  vim.api.nvim_buf_set_var(bufnr, var_name, nil)
 end
 
 M = {
-  DiagWinPreview = DiagWinPreview,
-  BufHoverPreview = function(opts)
+  wrap_float = function(var_name)
+    vim.lsp.util.open_floating_preview = float_wrapper(
+      vim.lsp.util.open_floating_preview,
+      var_name
+    )
+  end,
+  hover_toggle = function(opts, bufnr, var_name)
+    local result, hover_win_id = pcall(
+      vim.api.nvim_buf_get_var,
+      bufnr,
+      var_name
+    )
+    if result and hover_win_id and
+        vim.api.nvim_win_is_valid(hover_win_id) then
+      vim.api.nvim_win_close(hover_win_id, true)
+      vim.api.nvim_buf_set_var(bufnr, var_name, nil)
+      return
+    end
+    opts.parent_bufnr = bufnr
     vim.lsp.buf.hover(opts)
-    vim.cmd.sleep("20m")
-    DiagWinPreview()
+  end,
+  diag_win_preview = diag_win_preview,
+  buf_hover_preview = function(opts, var_name, bufnr)
+    if bufnr then
+      opts.parent_bufnr = bufnr
+    end
+    opts.callback = function()
+      diag_win_preview(bufnr, var_name)
+    end
+    vim.lsp.buf.hover(opts)
   end,
 }
 
