1735ca2ddf879e574c8ee7bdb663eab181ffcdbb
Rellikeht <michaelhemperek@gmail.com>
2025-11-29 17:46:47 +0100

    move variable name inside module

--- lua/nvim_lsp_config.lua
+++ lua/nvim_lsp_config.lua
@@ -1,9 +1,11 @@
 -- TODO document all this
 
+local hover_id_name = "lsp_config_hover_id"
+
 -- inspired by
 -- https://github.com/neovim/neovim/discussions/35953#discussioncomment-14544580
 -- this makes it possible to toggle hover window in insert mode
-local float_wrapper = function(opener, var_name)
+local float_wrapper = function(opener)
   return function(contents, syntax, opts)
     local buf_id, win_id = opener(contents, syntax, opts)
     if not buf_id or not win_id then
@@ -12,7 +14,7 @@ local float_wrapper = function(opener, var_name)
     if opts.parent_bufnr ~= nil then
       vim.api.nvim_buf_set_var(
         opts.parent_bufnr,
-        var_name,
+        hover_id_name,
         win_id
       )
     end
@@ -23,18 +25,18 @@ local float_wrapper = function(opener, var_name)
   end
 end
 
-local function diag_win_preview(bufnr, var_name)
-  local result, win_id = pcall(vim.api.nvim_buf_get_var, bufnr, var_name)
+local function diag_win_preview(bufnr)
+  local result, win_id = pcall(vim.api.nvim_buf_get_var, bufnr, hover_id_name)
   if not result or not win_id then
     return
   elseif not vim.api.nvim_win_is_valid(win_id) then
-    vim.api.nvim_buf_set_var(bufnr, var_name, nil)
+    vim.api.nvim_buf_set_var(bufnr, hover_id_name, nil)
     return
   end
   vim.api.nvim_set_current_win(win_id)
   print(vim.cmd("pbuffer %"))
   vim.api.nvim_win_close(win_id, true)
-  vim.api.nvim_buf_set_var(bufnr, var_name, nil)
+  vim.api.nvim_buf_set_var(bufnr, hover_id_name, nil)
   -- TODO clear highlight on close of preview window and if possible
   -- check if there was previous highlight to leave alone
   -- clears only current line
@@ -45,34 +47,33 @@ local function diag_win_preview(bufnr, var_name)
 end
 
 M = {
-  wrap_float = function(var_name)
+  wrap_float = function()
     vim.lsp.util.open_floating_preview = float_wrapper(
-      vim.lsp.util.open_floating_preview,
-      var_name
+      vim.lsp.util.open_floating_preview
     )
   end,
-  hover_toggle = function(opts, bufnr, var_name)
+  hover_toggle = function(opts, bufnr)
     local result, hover_win_id = pcall(
       vim.api.nvim_buf_get_var,
       bufnr,
-      var_name
+      hover_id_name
     )
     if result and hover_win_id and
         vim.api.nvim_win_is_valid(hover_win_id) then
       vim.api.nvim_win_close(hover_win_id, true)
-      vim.api.nvim_buf_set_var(bufnr, var_name, nil)
+      vim.api.nvim_buf_set_var(bufnr, hover_id_name, nil)
       return
     end
     opts.parent_bufnr = bufnr
     vim.lsp.buf.hover(opts)
   end,
   diag_win_preview = diag_win_preview,
-  buf_hover_preview = function(opts, var_name, bufnr)
+  buf_hover_preview = function(opts, bufnr)
     if bufnr then
       opts.parent_bufnr = bufnr
     end
     opts.callback = function()
-      diag_win_preview(bufnr, var_name)
+      diag_win_preview(bufnr)
     end
     vim.lsp.buf.hover(opts)
   end,
